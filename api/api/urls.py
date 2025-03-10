5f2cba59ead41cacc9ef5b3370f013f88e18ca4a
Marisol López <jennymlo2399@gmail.com>
2025-03-10 10:37:58 -0600

    feat: add card urls

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from painting import views
+from card import views
 
 urlpatterns = [
-    path('api/paintings', views.painting_list, name='painting_list'),  # View to obtain all the paintings
-    path('api/paintings/create', views.painting_create, name='painting_create'),  # View to create a painting
-    path('api/paintings/update/<int:id>', views.painting_update, name='painting_update'),  # View to update a painting
-    path('api/paintings/delete/<int:id>', views.painting_delete, name='painting_delete'),  # View to delete a painting
-    path('api/paintings/<int:id>', views.painting_detail, name='painting_detail'),  # View to obtain details of a painting by id
+    path('api/cards', views.card_list, name='card_list'),  # View to obtain all the cards
+    path('api/cards/create', views.card_create, name='card_create'),  # View to create a card
+    path('api/cards/update/<int:id>', views.card_update, name='card_update'),  # View to update a card
+    path('api/cards/delete/<int:id>', views.card_delete, name='card_delete'),  # View to delete a card
+    path('api/cards/<int:id>', views.card_detail, name='card_detail')  # View to obtain details of a card by id
 ]
